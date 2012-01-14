611151d1336e45b391bc4b6c3338aad72c8435df
Akash Mudubagilu <akashmudubagilu@gmail.com>
2012-01-14 18:09:13 -0500

    Initial Commit

---
+++ AMImageManager/AMImageManager.h
@@ -0,0 +1,13 @@
+//
+//  AMImageManager.h
+//  AMImageManager
+//
+//  Created by Akash Mudubagilu on 1/14/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AMImageManager : NSObject
+
+@end
---
+++ AMImageManager/AMImageManager.m
@@ -0,0 +1,23 @@
+//
+//  AMImageManager.m
+//  AMImageManager
+//
+//  Created by Akash Mudubagilu on 1/14/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "AMImageManager.h"
+
+@implementation AMImageManager
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
